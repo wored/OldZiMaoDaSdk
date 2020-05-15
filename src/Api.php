fbd0d9546c56667602b8b06b24a15f41db87e5dd
qiliu <qiliuf@gmail.com>
2020-05-15 16:49:07 +0800

    first

---
+++ src/Api.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Wored\ZiMaoDaSdk;
+
+
+use Hanson\Foundation\AbstractAPI;
+use Psr\Http\Message\ResponseInterface;
+
+class Api extends AbstractAPI
+{
+    public $config;
+    public $params;
+
+    /**
+     * Api constructor.
+     * @param $appkey
+     * @param $appsecret
+     * @param $sid
+     * @param $baseUrl
+     */
+    public function __construct(ZiMaoDaSdk $ziMaoDaSdk)
+    {
+        $this->config = $ziMaoDaSdk->getConfig();
+        $this->params = [
+            'nick'   => $this->config['nick'],//卖家店铺昵称
+            'name'   => $this->config['name'],//访问此接口所需要的帐号
+            'format' => $this->config['format'],//格式：xml
+        ];
+    }
+
+    /**
+     * @param string $urlPath
+     * @param array $params
+     * @return mixed
+     */
+    public function request(string $urlPath, array $params)
+    {
+        $http = $this->getHttp();
+        $params = array_merge($this->params, $params);
+        $params['date'] = date('Y-m-d H:i:s', time());//调用接口的时间
+        $params['sign'] = $this->sign($params);//请使用32位md5加密
+        $requestUrl = $this->config['rootUrl'] . '/webAPI/' . $urlPath;
+        $response = call_user_func_array([$http, 'POST'], [$requestUrl, $params]);
+        return json_decode(strval($response->getBody()), true);
+    }
+
+    /**
+     * 生成签名
+     * @param array $request_params
+     * @return string
+     */
+    public function sign(array $params)
+    {
+        $data = '';
+        $data .= base64_encode($params['nick']);
+        $data .= base64_encode($params['method']);
+        $data .= base64_encode($params['date']);
+        $data .= base64_encode($params['name']);
+        $data .= base64_encode($params['orderNumber']);
+        $data .= base64_encode($params['format']);
+        return md5($data);
+    }
+
+    /**
+     * @param $param
+     * @param bool $root
+     * @return string
+     */
+    public function paramToXml($param, $root = true)
+    {
+        if ($root) {
+            $xml = '<?xml version="1.0" encoding="utf-8"?>';
+        } else {
+            $xml = '';
+        }
+        foreach ($param as $key => $vo) {
+            if ($key === 'attributes') {//判断是否是属性字段
+                continue;
+            }
+            if (!is_numeric($key)) {
+                $xml .= "<{$key}";
+                if (!empty($vo['attributes'])) {//添加属性
+                    foreach ($vo['attributes'] as $item => $attribute) {
+                        $xml .= " {$item}=\"{$attribute}\"";
+                    }
+                }
+                $xml .= '>';
+            }
+            if (is_array($vo) and count($vo) > 0) {
+                $xml .= $this->paramToXml($vo, false);
+            } else {
+                $xml .= $vo;
+            }
+            if (!is_numeric($key)) {
+                $xml .= "</{$key}>";
+            }
+        }
+        return $xml;
+    }
+}
---
+++ src/ZiMaoDaSdk.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Wored\ZiMaoDaSdk;
+
+
+use Hanson\Foundation\Foundation;
+
+/***
+ * Class ZiMaoDaSdk
+ * @package \Wored\ZiMaoDaSdk
+ * 
+ * @property \Wored\ZiMaoDaSdk\Api $api
+ */
+class ZiMaoDaSdk extends Foundation
+{
+    protected $providers = [
+        ServiceProvider::class
+    ];
+
+    public function __construct($config)
+    {
+        $config['debug'] = $config['debug'] ?? false;
+        parent::__construct($config);
+    }
+
+    /**
+     * 订单信息创建到自贸达
+     * 客户网站上的订单创建到ZMD报关系统，采用post方式传输，传入的数据为xml文件
+     * @param array $order 以数组的信息拼装
+     * @return mixed
+     */
+    public function createOrder(array $order)
+    {
+        $params = [
+            'method'      => 'swapi.web.order.create',//方法名称
+            'orderDate'   => $order['OWebOrder']['orderDate'],//订单推送时间
+            'orderNumber' => $order['OWebOrder']['orderNumber'],//订单编号
+            'orderData'   => $this->api->paramToXml($order),//xml请求数据
+        ];
+        return $this->api->request('createOrder', $params);
+    }
+
+    /**
+     * 取消订单
+     * 客户订单从自贸达系统取消，采用post方式传输，传入的数据为xml文件
+     * @param array $order
+     * @return mixed
+     */
+    public function refundOrder(array $order)
+    {
+        $params = [
+            'method'      => 'swapi.web.order.refund',//方法名称
+            'orderNumber' => $order['OWebOrderReFund']['orderNumber'],//订单编号
+            'orderData'   => $this->api->paramToXml($order),//xml请求数据
+        ];
+        return $this->api->request('refundOrder', $params);
+    }
+}
