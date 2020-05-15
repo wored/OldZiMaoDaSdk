9b9ebd71988a260d481d5781bd74af8450e2c837
qiliu <qiliuf@gmail.com>
2020-05-15 16:59:01 +0800

    delete

--- src/Helper.php
+++ src/Helper.php
@@ -5,16 +5,5 @@ namespace Wored\OldZiMaoDaSdk;
 
 class Helper
 {
-    static public function dump($param)
-    {
-        echo '<pre>';
-        var_dump($param);
-        echo '</pre>';
-    }
 
-    static public function dd($param)
-    {
-        self::dump($param);
-        exit;
-    }
 }
